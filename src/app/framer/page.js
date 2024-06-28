17af4f3c3d7cd28d4ff6df27539056184394b0bd
WebWithRathor <devrajrathor143@gmail.com>
2024-06-28 13:24:06 +0530

    Animation

---
+++ src/app/framer/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>framer</div>
+  )
+}
+
+export default page
---
+++ src/app/gsap/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>gsap</div>
+  )
+}
+
+export default page
